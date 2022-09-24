df2e757faabd0d8b07cd8b8dae672424776eace9
dysby <>
2022-09-24 11:19:07 +0100

    add flow rule fuctions instead of subclassing

---
+++ Assigment1/flow_rules.py
@@ -0,0 +1,136 @@
+from typing import Tuple
+
+from .tiles import Flow
+
+
+def follow_initial_left(
+    loc: Tuple[int, int], flow: Flow
+) -> Tuple[Tuple[int, int], Flow]:
+    return ((loc[0], loc[1] - 1), Flow.RIGHT)
+
+
+def follow_initial_right(
+    loc: Tuple[int, int], flow: Flow
+) -> Tuple[Tuple[int, int], Flow]:
+    return ((loc[0], loc[1] + 1), Flow.LEFT)
+
+
+def follow_initial_top(
+    loc: Tuple[int, int], flow: Flow
+) -> Tuple[Tuple[int, int], Flow]:
+    return ((loc[0] + 1, loc[1]), Flow.DOWN)
+
+
+def follow_initial_down(
+    loc: Tuple[int, int], flow: Flow
+) -> Tuple[Tuple[int, int], Flow]:
+    return ((loc[0] - 1, loc[1]), Flow.TOP)
+
+
+def follow_goal_left(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.LEFT:
+        return (loc, flow)
+    return (loc, Flow.ERROR)
+
+
+def follow_goal_right(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.RIGHT:
+        return (loc, flow)
+    return (loc, Flow.ERROR)
+
+
+def follow_goal_top(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.TOP:
+        return (loc, flow)
+    return (loc, Flow.ERROR)
+
+
+def follow_goal_down(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.DOWN:
+        return (loc, flow)
+    return (loc, Flow.ERROR)
+
+
+def follow_right_left(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.LEFT:
+        return ((loc[0], loc[1] + 1), Flow.LEFT)
+    elif flow == Flow.RIGHT:
+        return ((loc[0], loc[1] - 1), Flow.RIGHT)
+    else:
+        return (loc, Flow.ERROR)
+
+
+def follow_top_down(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.TOP:
+        return ((loc[0] - 1, loc[1]), Flow.TOP)
+    elif flow == Flow.DOWN:
+        return ((loc[0] + 1, loc[1] + 1), Flow.DOWN)
+    else:
+        return (loc, Flow.ERROR)
+
+
+def follow_right_top(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.RIGHT:
+        return ((loc[0] - 1, loc[1]), Flow.DOWN)
+    elif flow == Flow.TOP:
+        return ((loc[0], loc[1] + 1), Flow.LEFT)
+    else:
+        return (loc, Flow.ERROR)
+
+
+def follow_right_down(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.RIGHT:
+        return ((loc[0] + 1, loc[1]), Flow.TOP)
+    elif flow == Flow.DOWN:
+        return ((loc[0], loc[1] + 1), Flow.LEFT)
+    else:
+        return (loc, Flow.ERROR)
+
+
+def follow_left_top(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.LEFT:
+        return ((loc[0] - 1, loc[1]), Flow.DOWN)
+    elif flow == Flow.TOP:
+        return ((loc[0], loc[1] - 1), Flow.RIGHT)
+    else:
+        return (loc, Flow.ERROR)
+
+
+def follow_left_down(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.LEFT:
+        return ((loc[0] + 1, loc[1]), Flow.TOP)
+    elif flow == Flow.DOWN:
+        return ((loc[0], loc[1] - 1), Flow.RIGHT)
+    else:
+        return (loc, Flow.ERROR)
+
+
+def follow_no_passage(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    return (loc, Flow.ERROR)
+
+
+follow_func = {
+    "initial-left": follow_initial_left,
+    "initial-right": follow_initial_right,
+    "initial-top": follow_initial_top,
+    "initial-down": follow_initial_down,
+    "goal-left": follow_goal_left,
+    "goal-right": follow_goal_right,
+    "goal-top": follow_goal_top,
+    "goal-down": follow_goal_down,
+    "right-left-not": follow_right_left,
+    "top-down-not": follow_top_down,
+    "right-top-not": follow_right_top,
+    "right-down-not": follow_right_down,
+    "left-top-not": follow_left_top,
+    "left-down-not": follow_left_down,
+    "no-passage-not": follow_no_passage,
+    "right-left": follow_right_left,
+    "top-down": follow_top_down,
+    "right-top": follow_right_top,
+    "right-down": follow_right_down,
+    "left-top": follow_left_top,
+    "left-down": follow_left_down,
+    "no-passage": follow_no_passage,
+    "empty-cell": follow_no_passage,
+}
