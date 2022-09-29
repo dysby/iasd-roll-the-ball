6e226bb2b799570de11bbe5e280c5431098b0cca
dysby <>
2022-09-29 18:47:45 +0100

    merge flow_rules to solution file

--- Assigment1/solution.py
+++ Assigment1/solution.py
@@ -1,15 +1,166 @@
+from typing import Tuple
+from enum import Enum, auto
+
 import search
-from flow_rules import Flow, follow_func
+
+
+class Flow(Enum):
+    """
+    *Flow* is the direction of an imaginary ball ariving at the current tile, 
+    following a correct path from the initial tile.
+
+    For example, if we are on a tile with a exit to the right, the flow will become left, 
+    because the next tile will see the ball comming from the left.
+    """
+
+    TOP = auto()
+    DOWN = auto()
+    RIGHT = auto()
+    LEFT = auto()
+    ERROR = auto()
+
+
+def follow_initial_left(
+    loc: Tuple[int, int], flow: Flow
+) -> Tuple[Tuple[int, int], Flow]:
+    return ((loc[0], loc[1] - 1), Flow.RIGHT)
+
+
+def follow_initial_right(
+    loc: Tuple[int, int], flow: Flow
+) -> Tuple[Tuple[int, int], Flow]:
+    return ((loc[0], loc[1] + 1), Flow.LEFT)
+
+
+def follow_initial_top(
+    loc: Tuple[int, int], flow: Flow
+) -> Tuple[Tuple[int, int], Flow]:
+    return ((loc[0] + 1, loc[1]), Flow.DOWN)
+
+
+def follow_initial_down(
+    loc: Tuple[int, int], flow: Flow
+) -> Tuple[Tuple[int, int], Flow]:
+    return ((loc[0] - 1, loc[1]), Flow.TOP)
+
+
+def follow_goal_left(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.LEFT:
+        return (loc, flow)
+    return (loc, Flow.ERROR)
+
+
+def follow_goal_right(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.RIGHT:
+        return (loc, flow)
+    return (loc, Flow.ERROR)
+
+
+def follow_goal_top(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.TOP:
+        return (loc, flow)
+    return (loc, Flow.ERROR)
+
+
+def follow_goal_down(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.DOWN:
+        return (loc, flow)
+    return (loc, Flow.ERROR)
+
+
+def follow_right_left(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.LEFT:
+        return ((loc[0], loc[1] + 1), Flow.LEFT)
+    elif flow == Flow.RIGHT:
+        return ((loc[0], loc[1] - 1), Flow.RIGHT)
+    else:
+        return (loc, Flow.ERROR)
+
+
+def follow_top_down(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.TOP:
+        return ((loc[0] - 1, loc[1]), Flow.TOP)
+    elif flow == Flow.DOWN:
+        return ((loc[0] + 1, loc[1] + 1), Flow.DOWN)
+    else:
+        return (loc, Flow.ERROR)
+
+
+def follow_right_top(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.RIGHT:
+        return ((loc[0] - 1, loc[1]), Flow.DOWN)
+    elif flow == Flow.TOP:
+        return ((loc[0], loc[1] + 1), Flow.LEFT)
+    else:
+        return (loc, Flow.ERROR)
+
+
+def follow_right_down(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.RIGHT:
+        return ((loc[0] + 1, loc[1]), Flow.TOP)
+    elif flow == Flow.DOWN:
+        return ((loc[0], loc[1] + 1), Flow.LEFT)
+    else:
+        return (loc, Flow.ERROR)
+
+
+def follow_left_top(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.LEFT:
+        return ((loc[0] - 1, loc[1]), Flow.DOWN)
+    elif flow == Flow.TOP:
+        return ((loc[0], loc[1] - 1), Flow.RIGHT)
+    else:
+        return (loc, Flow.ERROR)
+
+
+def follow_left_down(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    if flow == Flow.LEFT:
+        return ((loc[0] + 1, loc[1]), Flow.TOP)
+    elif flow == Flow.DOWN:
+        return ((loc[0], loc[1] - 1), Flow.RIGHT)
+    else:
+        return (loc, Flow.ERROR)
+
+
+def follow_no_passage(loc: Tuple[int, int], flow: Flow) -> Tuple[Tuple[int, int], Flow]:
+    return (loc, Flow.ERROR)
+
+
+follow_func = {
+    "initial-left": follow_initial_left,
+    "initial-right": follow_initial_right,
+    "initial-top": follow_initial_top,
+    "initial-down": follow_initial_down,
+    "goal-left": follow_goal_left,
+    "goal-right": follow_goal_right,
+    "goal-top": follow_goal_top,
+    "goal-down": follow_goal_down,
+    "right-left-not": follow_right_left,
+    "top-down-not": follow_top_down,
+    "right-top-not": follow_right_top,
+    "right-down-not": follow_right_down,
+    "left-top-not": follow_left_top,
+    "left-down-not": follow_left_down,
+    "no-passage-not": follow_no_passage,
+    "right-left": follow_right_left,
+    "top-down": follow_top_down,
+    "right-top": follow_right_top,
+    "right-down": follow_right_down,
+    "left-top": follow_left_top,
+    "left-down": follow_left_down,
+    "no-passage": follow_no_passage,
+    "empty-cell": follow_no_passage,
+}
 
 
 class RTBProblem(search.Problem):
-    def init(self):
+    def __init__(self):
         """
         Method that instantiate your class. You can change the content of this. self.initial is where
         the initial state of the puzzle should be saved.
         init initial state with empty tupple
         """
-        super().__init__(())
+        self.initial = ()
         self.N = 0
 
     def load(self, fh):
@@ -34,10 +185,10 @@ class RTBProblem(search.Problem):
     def isSolution(self):
         """returns 1 if the loaded puzzle is a solution, 0 otherwise."""
         board = self.initial
-        print(board)
+        # print(board)
 
         def _find_init():
-            # Locate the initial tile on the board, and set initial flow
+            """Locate the initial tile on the board, and set initial flow."""
             for idx, tile in enumerate(board):
                 if tile in (
                     "initial-left",
@@ -51,12 +202,12 @@ class RTBProblem(search.Problem):
         # initial position, flow is not defined, can be any value
         current_loc, flow = _find_init(), Flow.DOWN
 
-        print(current_loc, board[current_loc[0] * self.N + current_loc[1]])
+        # print(current_loc, board[current_loc[0] * self.N + current_loc[1]])
         while True:
             current_loc, flow = follow_func[
                 board[current_loc[0] * self.N + current_loc[1]]
             ](current_loc, flow)
-            print(flow, current_loc, board[current_loc[0] * self.N + current_loc[1]])
+            # print(flow, current_loc, board[current_loc[0] * self.N + current_loc[1]])
 
             # tile is not compatible: broke the flow or flows outside
             if (
