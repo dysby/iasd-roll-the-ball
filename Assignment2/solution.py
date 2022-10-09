87754c6456264592ef4cb6c0f7b8b19532d3f1d2
dysby <>
2022-10-09 16:29:27 +0100

    rename Assignment2

--- Assignment2/solution.py
+++ Assignment2/solution.py
@@ -4,12 +4,18 @@ from enum import Enum, auto
 import search
 
 
+class Direction(Enum):
+    UP = auto()
+    DOWN = auto()
+    LEFT = auto()
+    RIGTH = auto()
+
 class Flow(Enum):
     """
-    *Flow* is the direction of an imaginary ball ariving at the current tile, 
+    *Flow* is the direction of an imaginary ball ariving at the current tile,
     following a correct path from the initial tile.
 
-    For example, if we are on a tile with a exit to the right, the flow will become left, 
+    For example, if we are on a tile with a exit to the right, the flow will become left,
     because the next tile will see the ball comming from the left.
     """
 
@@ -18,6 +24,33 @@ class Flow(Enum):
     RIGHT = auto()
     LEFT = auto()
     ERROR = auto()
+
+class Tile(Enum):
+    INITIAL_LEFT = "initial-left"
+    INITIAL_RIGHT = "initial-right"
+    INITIAL_TOP = "initial-top"
+    INITIAL_DOWN = "initial-down"
+    GOAL_LEFT = "goal-left"
+    GOAL_RIGHT = ""
+    GOAL_TOP = "goal-top"
+    GOAL_DOWN = "goal-down"
+    RIGHT_LEFT_NOT = "right-left-not"
+    TOP_DOWN_NOT = "top-down-not"
+    RIGHT_TOP_NOT = "right-top-not"
+    RIGHT_DOWN_NOT = "right-down-not"
+    LEFT_TOP_NOT = "left-top-not"
+    LEFT_DOWN_NOT = "left-down-not"
+    NO_PASSAGE_NOT = "no-passage-not"
+    RIGHT_LEFT = "right-left"
+    TOP_DOWN = "top-down"
+    RIGHT_TOP = "right-top"
+    RIGHT_DOWN = "right-down"
+    LEFT_TOP = "left-top"
+    LEFT_DOWN = "left-down"
+    NO_PASSAGE = "no-passage"
+    EMPTY_CELL = "empty-cell"
+
+
 
 
 def follow_initial_left(
@@ -153,9 +186,62 @@ follow_func = {
 }
 
 initial_tile_types = {"initial-left", "initial-right", "initial-top", "initial-down"}
-goal_tile_types = { "goal-left", "goal-right", "goal-top", "goal-down"}
+goal_tile_types = {"goal-left", "goal-right", "goal-top", "goal-down"}
 # use | for set union and & for set intersection
-unmovable_tile_types = initial_tile_types | goal_tile_types | { "right-left-not", "top-down-not", "right-top-not", "right-down-not", "left-top-not", "left-down-not", "no-passage-not"}
+unmovable_tile_types = (
+    initial_tile_types
+    | goal_tile_types
+    | {
+        "right-left-not",
+        "top-down-not",
+        "right-top-not",
+        "right-down-not",
+        "left-top-not",
+        "left-down-not",
+        "no-passage-not",
+    }
+)
+
+
+compatible_tiles = {
+    "initial-left": [
+        "right-top",
+        "right-top-not",
+        "right-down",
+        "right-down-not",
+        "right-left",
+        "right-left-not",
+    ],
+    "initial-right": [
+        "left-top",
+        "left-top-not",
+        "left-down",
+        "left-down-not",
+        "right-left",
+        "right-left-not",
+    ],
+    "initial-top": [],
+    "initial-down": [],
+    "goal-left": [],
+    "goal-right": [],
+    "goal-top": [],
+    "goal-down": [],
+    "right-left-not": [],
+    "top-down-not": [],
+    "right-top-not": [],
+    "right-down-not": [],
+    "left-top-not": [],
+    "left-down-not": [],
+    "no-passage-not": [],
+    "right-left": [],
+    "top-down": [],
+    "right-top": [],
+    "right-down": [],
+    "left-top": [],
+    "left-down": [],
+    "no-passage": [],
+    "empty-cell": [],
+}
 
 
 class RTBProblem(search.Problem):
@@ -165,7 +251,8 @@ class RTBProblem(search.Problem):
         the initial state of the puzzle should be saved.
         init initial state with empty tupple
         """
-        self.initial = ()
+        self.initial = None
+        self.algorithm = None
         self.N = 0
 
     def load(self, fh):
@@ -194,7 +281,7 @@ class RTBProblem(search.Problem):
         board = self.initial
         # hack for pub10
         if self.N == 0 and len(board) > 0:
-            self.N = int(len(board)**0.5)
+            self.N = int(len(board) ** 0.5)
 
         def _find_init():
             """Locate the initial tile on the board, and set initial flow."""
@@ -236,3 +323,74 @@ class RTBProblem(search.Problem):
                 if flow == Flow.ERROR:
                     return 0
                 return 1
+
+    def result(self, state, action):
+        """Return the state that results from executing the given action in the given state."""
+        pass
+
+    def actions(self, state):
+        """Return the actions that can be executed in the given state."""
+        pass
+
+    def _find_init(self, state) -> Tuple[int, int]:
+            """Locate the initial tile on the state, and set initial flow."""
+            for idx, tile in enumerate(state):
+                if tile in initial_tile_types:
+                    return (idx // self.N, idx % self.N)
+            raise ValueError("did not find initial tile")
+
+    def goal_test(self, state) -> bool:
+        """Return True if the state is a goal."""
+        if self.N == 0 and len(state) > 0:
+            self.N = int(len(state) ** 0.5)
+
+        # initial position, flow will not be defined, can be any value
+        current_loc, flow = self._find_init(state), Flow.DOWN
+
+        # print(current_loc, state)
+        while True:
+            current_loc, flow = follow_func[
+                state[current_loc[0] * self.N + current_loc[1]]
+            ](current_loc, flow)
+            # print(flow, current_loc, state[current_loc[0] * self.N + current_loc[1]])
+
+            # tile is not compatible: broke the flow or flows outside
+            if (
+                flow == Flow.ERROR
+                or current_loc[0] < 0
+                or current_loc[0] >= self.N
+                or current_loc[1] < 0
+                or current_loc[1] >= self.N
+            ):
+                return False
+
+            # reached another initial tile, not solvable
+            if state[current_loc[0] * self.N + current_loc[1]] in initial_tile_types:
+                return False
+
+            # found a goal tile, is it compatible?
+            if state[current_loc[0] * self.N + current_loc[1]] in goal_tile_types:
+                # final flow test to check if goal tile is compatible
+                current_loc, flow = follow_func[
+                    state[current_loc[0] * self.N + current_loc[1]]
+                ](current_loc, flow)
+                if flow == Flow.ERROR:
+                    return False
+                return True
+
+    def setAlgorithm(self):
+        """Sets the uninformed search algorithm chosen."""
+        self.algorithm = search.breadth_first_tree_search
+        # example : self.algorithm = search.breadth_first_tree_search
+        # substitute by the function in search.py that
+        # implements the chosen algorithm.
+        # You can only use the algorithms defined in search.py
+
+    def solve(self):
+        """Calls the uninformed search algorithm chosen."""
+        return self.algorithm(self, ...)
+        # You have to provide the arguments for the
+        # chosen algorithm if any.
+        # For instance , for the Depth Limited Search you need to
+        # provide a value for the limit L, otherwise the default
+        # value (50) will be used.
---
+++ Assignment2/test_assignment2.py
@@ -0,0 +1,64 @@
+from solution import RTBProblem 
+
+def test_public_01():
+    with open("public_tests/pub01.dat") as fh:
+        problem = RTBProblem()
+        problem.load(fh)
+        solution = problem.isSolution()
+        assert solution == 1
+
+def test_public_02():
+    with open("public_tests/pub02.dat") as fh:
+        problem = RTBProblem()
+        problem.load(fh)
+        solution = problem.isSolution()
+        assert solution == 0
+def test_public_03():
+    with open("public_tests/pub03.dat") as fh:
+        problem = RTBProblem()
+        problem.load(fh)
+        solution = problem.isSolution()
+        assert solution == 1
+def test_public_04():
+    with open("public_tests/pub04.dat") as fh:
+        problem = RTBProblem()
+        problem.load(fh)
+        solution = problem.isSolution()
+        assert solution == 0
+def test_public_05():
+    with open("public_tests/pub05.dat") as fh:
+        problem = RTBProblem()
+        problem.load(fh)
+        solution = problem.isSolution()
+        assert solution == 1
+def test_public_06():
+    with open("public_tests/pub06.dat") as fh:
+        problem = RTBProblem()
+        problem.load(fh)
+        solution = problem.isSolution()
+        assert solution == 0
+def test_public_07():
+    with open("public_tests/pub07.dat") as fh:
+        problem = RTBProblem()
+        problem.load(fh)
+        solution = problem.isSolution()
+        assert solution == 1
+def test_public_08():
+    with open("public_tests/pub08.dat") as fh:
+        problem = RTBProblem()
+        problem.load(fh)
+        solution = problem.isSolution()
+        assert solution == 0
+def test_public_09():
+    with open("public_tests/pub09.dat") as fh:
+        problem = RTBProblem()
+        problem.load(fh)
+        solution = problem.isSolution()
+        assert solution == 1
+def test_public_10():
+    with open("public_tests/teste_1001_snake.dat") as fh:
+        problem = RTBProblem()
+        problem.load(fh)
+        solution = problem.isSolution()
+        assert solution == 0
+
